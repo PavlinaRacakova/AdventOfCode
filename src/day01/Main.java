09802b82ec476d13de72eedd877116c4f52683f2
PavlinaRacakova <pavlina.kupkova@email.cz>
2023-12-04 21:30:38 +0700

    day 01

--- src/day01/Main.java
+++ src/day01/Main.java
@@ -10,20 +10,20 @@ public class Main {
         String[] lines = sc.useDelimiter("\\A").next().split("\n");
         long finalResult = 0;
 
-        for(String line : lines) {
+        for (String line : lines) {
             finalResult += Integer.parseInt(findCoordinates(line));
         }
 
         System.out.println(finalResult);
+        sc.close();
     }
 
     public static String findCoordinates(String line) {
         line = replaceWrittenDigits(line);
-        int firstCoordinate = Integer.parseInt(String.valueOf(line.charAt(firstIndexOfRegex(line))));
-        StringBuilder builder = new StringBuilder(line);
-        builder.reverse();
-        int secondCoordinate = Integer.parseInt(String.valueOf(builder.charAt(firstIndexOfRegex(String.valueOf(builder)))));
-        return String.valueOf(firstCoordinate) + secondCoordinate;
+        String reversedLine = reverseGivenLine(line);
+        String firstCoordinate = String.valueOf(line.charAt(firstIndexOfRegex(line)));
+        String secondCoordinate = String.valueOf(reversedLine.charAt(firstIndexOfRegex(reversedLine)));
+        return firstCoordinate + secondCoordinate;
     }
 
     public static int firstIndexOfRegex(String line) {
@@ -31,8 +31,7 @@ public class Main {
         Matcher matcher = pattern.matcher(line);
         int index = 0;
 
-        if (matcher.find())
-        {
+        if (matcher.find()) {
             index = matcher.start();
         }
 
@@ -40,15 +39,21 @@ public class Main {
     }
 
     public static String replaceWrittenDigits(String line) {
-        line = line.replaceAll("one", "o1e");
-        line = line.replaceAll("two", "t2o");
-        line = line.replaceAll("three", "th3ee");
-        line = line.replaceAll("four", "f4ur");
-        line = line.replaceAll("five", "f5ve");
-        line = line.replaceAll("six", "s6x");
-        line = line.replaceAll("seven", "se7en");
-        line = line.replaceAll("eight", "ei8ht");
-        line = line.replaceAll("nine", "n9ine");
+        String[] replacements = {
+                "one", "o1e", "two", "t2o", "three", "s3e", "four", "f4r",
+                "five", "f5e", "six", "s6x", "seven", "s7n", "eight", "e8t", "nine", "n9e"
+        };
+
+        for (int i = 0; i < replacements.length; i += 2) {
+            line = line.replaceAll(replacements[i], replacements[i + 1]);
+        }
+
         return line;
+    }
+
+    public static String reverseGivenLine(String line) {
+        StringBuilder builder = new StringBuilder(line);
+        builder.reverse();
+        return String.valueOf(builder);
     }
 }
